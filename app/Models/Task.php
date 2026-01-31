d14e8d70a6376eed522660df98d4c39dc81f1981
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-01-31 21:54:22 +0100

    feat(model): add model

---
+++ app/Models/Task.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use SoftDeletes;
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'priority',
+        'status',
+        'deadline',
+        'is_archived'
+    ];
+
+    protected $casts = [
+        'deadline' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
